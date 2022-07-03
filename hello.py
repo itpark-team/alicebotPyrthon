2830cebac18af9f4b77b6e15741e701a9f82312e
itpark <a.m.smyslov@yandex.ru>
2022-07-03 17:34:45 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import logging
+import json
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route("/", methods=["POST"])
+def main():
+    logging.info(request.json)
+
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+    else:
+        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
+            response["response"]["text"] = "Супер! Я за вас рада!"
+        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+
+    return json.dumps(response)
+
+
+if __name__ == "__main__":
+    app.run()
